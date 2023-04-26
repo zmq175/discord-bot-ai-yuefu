83be87d07c325d99b2277ea0872b7f0b23f4ec36
Chengzhi <zmq175@qq.com>
2023-04-26 22:30:59 +0800

    try to await

--- commands/speak.js
+++ commands/speak.js
@@ -11,6 +11,49 @@ module.exports = {
             .setName('text')
             .setDescription('要说什么')
             .setRequired(true)),
+
+    async speakAndChangeVoice(text) {
+        const { AudioConfig, SpeechSynthesizer, ResultReason } = require('microsoft-cognitiveservices-speech-sdk');
+        const config = SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SERVICE_REGION);
+        config.speechSynthesisLanguage = 'zh-CN';
+        const synthesizer = new SpeechSynthesizer(config, AudioConfig.fromAudioFileOutput('./output.wav'));
+
+        return new Promise((resolve, reject) => {
+            synthesizer.speakTextAsync(
+                text,
+                async (result) => {
+                    if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+                        console.log(result.audioData.byteLength);
+                        const request = require('request');
+                        const formData = {
+                            sample: {
+                                value: Buffer.from(result.audioData),
+                                options: {
+                                    filename: 'output.wav',
+                                    contentType: 'audio/wav'
+                                }
+                            },
+                            fPitchChange: '1',
+                            sampleRate: '44100'
+                        };
+                        const options = {
+                            url: 'http://121.41.44.246:7860/voiceChangeModel',
+                            formData: formData,
+                            headers: { Authorization: process.env.AUTH },
+                        };
+                        request.post(options, async (error, response, body) => {
+                            resolve(Buffer.from(body));
+                        });
+                    }
+                    synthesizer.close();
+                },
+                (error) => {
+                    reject(error);
+                    synthesizer.close();
+                }
+            );
+        });
+    },
     /**
      * 
      * @param {ChatInputCommandInteraction} interaction 
@@ -21,44 +64,7 @@ module.exports = {
             fetchReply: true
         });
 
-        // 设置语音转换器的配置
-        const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
-
-        speechConfig.speechSynthesisOutputFormat = sdk.SpeechSynthesisOutputFormat.Audio16Khz16BitMonoWav;
-        // 创建语音合成器
-        const synthesizer = new sdk.SpeechSynthesizer(speechConfig, null);
-
-        synthesizer.speakTextAsync(
-            text,
-            async (result) => {
-                if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-                    console.log(result.audioData.byteLength);
-                    const request = require('request');
-                    const formData = {
-                        sample: {
-                            value: Buffer.from(result.audioData),
-                            options: {
-                                filename: 'output.wav',
-                                contentType: 'audio/wav'
-                            }
-                        },
-                        fPitchChange: '1',
-                        sampleRate: '44100'
-                    };
-                    const options = {
-                        url: 'http://121.41.44.246:7860/voiceChangeModel',
-                        formData: formData,
-                        headers: { Authorization: process.env.AUTH },
-                    };
-                    request.post(options, async (error, response, body) => {
-                        await interaction.editReply({ files: [{ attachment: Buffer.from(body), name: 'output.wav' }] });
-                    });
-                }
-                synthesizer.close();
-            },
-            error => {
-                console.log(error);
-                synthesizer.close();
-            });
+        const buffer = await speakAndChangeVoice(text);
+        await interaction.editReply({ files: [{ attachment: buffer, name: 'output.wav' }] });
     }
 }
