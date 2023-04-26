9f9eb24a0d06c75d6298aa4c0f47d176077e0acc
Chengzhi <zmq175@qq.com>
2023-04-26 22:32:43 +0800

    change

--- commands/speak.js
+++ commands/speak.js
@@ -3,6 +3,49 @@ const fs = require('fs');
 const sdk = require('microsoft-cognitiveservices-speech-sdk');
 const request = require('request');
 
+async function speakAndChangeVoice(text) {
+  const { AudioConfig, SpeechSynthesizer, ResultReason } = require('microsoft-cognitiveservices-speech-sdk');
+  const config = SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SERVICE_REGION);
+  config.speechSynthesisLanguage = 'zh-CN';
+  const synthesizer = new SpeechSynthesizer(config, AudioConfig.fromAudioFileOutput('./output.wav'));
+
+  return new Promise((resolve, reject) => {
+      synthesizer.speakTextAsync(
+          text,
+          async (result) => {
+              if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+                  console.log(result.audioData.byteLength);
+                  const request = require('request');
+                  const formData = {
+                      sample: {
+                          value: Buffer.from(result.audioData),
+                          options: {
+                              filename: 'output.wav',
+                              contentType: 'audio/wav'
+                          }
+                      },
+                      fPitchChange: '1',
+                      sampleRate: '44100'
+                  };
+                  const options = {
+                      url: 'http://121.41.44.246:7860/voiceChangeModel',
+                      formData: formData,
+                      headers: { Authorization: process.env.AUTH },
+                  };
+                  request.post(options, async (error, response, body) => {
+                      resolve(Buffer.from(body));
+                  });
+              }
+              synthesizer.close();
+          },
+          (error) => {
+              reject(error);
+              synthesizer.close();
+          }
+      );
+  });
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('speak')
@@ -11,49 +54,6 @@ module.exports = {
             .setName('text')
             .setDescription('要说什么')
             .setRequired(true)),
-
-    async speakAndChangeVoice(text) {
-        const { AudioConfig, SpeechSynthesizer, ResultReason } = require('microsoft-cognitiveservices-speech-sdk');
-        const config = SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SERVICE_REGION);
-        config.speechSynthesisLanguage = 'zh-CN';
-        const synthesizer = new SpeechSynthesizer(config, AudioConfig.fromAudioFileOutput('./output.wav'));
-
-        return new Promise((resolve, reject) => {
-            synthesizer.speakTextAsync(
-                text,
-                async (result) => {
-                    if (result.reason === ResultReason.SynthesizingAudioCompleted) {
-                        console.log(result.audioData.byteLength);
-                        const request = require('request');
-                        const formData = {
-                            sample: {
-                                value: Buffer.from(result.audioData),
-                                options: {
-                                    filename: 'output.wav',
-                                    contentType: 'audio/wav'
-                                }
-                            },
-                            fPitchChange: '1',
-                            sampleRate: '44100'
-                        };
-                        const options = {
-                            url: 'http://121.41.44.246:7860/voiceChangeModel',
-                            formData: formData,
-                            headers: { Authorization: process.env.AUTH },
-                        };
-                        request.post(options, async (error, response, body) => {
-                            resolve(Buffer.from(body));
-                        });
-                    }
-                    synthesizer.close();
-                },
-                (error) => {
-                    reject(error);
-                    synthesizer.close();
-                }
-            );
-        });
-    },
     /**
      * 
      * @param {ChatInputCommandInteraction} interaction 
