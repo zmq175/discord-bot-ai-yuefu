a596f2c31988bcd262d3c7511e07e266b3ef701e
Chengzhi <zmq175@qq.com>
2023-04-26 23:36:52 +0800

    change

--- commands/speak.js
+++ commands/speak.js
@@ -34,7 +34,7 @@ async function speakAndChangeVoice(text) {
 
           try {
             const response = await axios.post('http://121.41.44.246:7860/voiceChangeModel', form, { headers });
-            resolve(new Blob([response], {
+            resolve(new Blob([response.data], {
               type: 'audio/x-wav'
             }));
           } catch (error) {
