71e4b9ab4eb02701dc38b42c694f6828131a7367
Chengzhi <zmq175@qq.com>
2023-04-26 22:55:00 +0800

    use stream

--- commands/speak.js
+++ commands/speak.js
@@ -10,61 +10,71 @@ async function speakAndChangeVoice(text) {
   const synthesizer = new SpeechSynthesizer(config, AudioConfig.fromAudioFileOutput('./output.wav'));
 
   return new Promise((resolve, reject) => {
-      synthesizer.speakTextAsync(
-          text,
-          async (result) => {
-              if (result.reason === ResultReason.SynthesizingAudioCompleted) {
-                  console.log(result.audioData.byteLength);
-                  const request = require('request');
-                  const formData = {
-                      sample: {
-                          value: Buffer.from(result.audioData),
-                          options: {
-                              filename: 'output.wav',
-                              contentType: 'audio/wav'
-                          }
-                      },
-                      fPitchChange: '1',
-                      sampleRate: '44100'
-                  };
-                  const options = {
-                      url: 'http://121.41.44.246:7860/voiceChangeModel',
-                      formData: formData,
-                      headers: { Authorization: process.env.AUTH },
-                  };
-                  request.post(options, async (error, response, body) => {
-                      resolve(body);
-                  });
+    synthesizer.speakTextAsync(
+      text,
+      async (result) => {
+        if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+          console.log(result.audioData.byteLength);
+          const request = require('request');
+          const formData = {
+            sample: {
+              value: Buffer.from(result.audioData),
+              options: {
+                filename: 'output.wav',
+                contentType: 'audio/wav'
               }
-              synthesizer.close();
-          },
-          (error) => {
-              reject(error);
-              synthesizer.close();
-          }
-      );
+            },
+            fPitchChange: '1',
+            sampleRate: '44100'
+          };
+          const options = {
+            url: 'http://121.41.44.246:7860/voiceChangeModel',
+            formData: formData,
+            headers: { Authorization: process.env.AUTH },
+          };
+          request.post(options, async (error, response, body) => {
+            resolve(Buffer.from(body));
+          });
+        }
+        synthesizer.close();
+      },
+      (error) => {
+        reject(error);
+        synthesizer.close();
+      }
+    );
   });
 }
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('speak')
-        .setDescription('让乐府说话')
-        .addStringOption(option => option
-            .setName('text')
-            .setDescription('要说什么')
-            .setRequired(true)),
-    /**
-     * 
-     * @param {ChatInputCommandInteraction} interaction 
-     */
-    async execute(interaction) {
-        const text = interaction.options.getString('text');
-        const message = await interaction.deferReply({
-            fetchReply: true
-        });
+  data: new SlashCommandBuilder()
+    .setName('speak')
+    .setDescription('让乐府说话')
+    .addStringOption(option => option
+      .setName('text')
+      .setDescription('要说什么')
+      .setRequired(true)),
+  /**
+   * 
+   * @param {ChatInputCommandInteraction} interaction 
+   */
+  async execute(interaction) {
+    const text = interaction.options.getString('text');
+    const message = await interaction.deferReply({
+      fetchReply: true
+    });
 
-        const buffer = await speakAndChangeVoice(text);
-        await interaction.editReply({ files: [{ attachment: buffer, name: 'output.wav', contentType: 'audio/x-wav' }] });
-    }
+    const buffer = await speakAndChangeVoice(text);
+    const filename = 'output.wav';
+    await promisify(fs.createWriteStream(filename)).write(buffer);
+
+    const fileStream = createReadStream(filename);
+
+    await interaction.editReply({
+      files: [{
+        attachment: fileStream,
+        name: fileName
+      }]
+    });
+  }
 }
