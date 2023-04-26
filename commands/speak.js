f42b73956dd0e5a82fa9a895d8d027d68f682a39
Chengzhi <zmq175@qq.com>
2023-04-26 23:09:08 +0800

    change to axios

--- commands/speak.js
+++ commands/speak.js
@@ -2,6 +2,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, ChatI
 const fs = require('fs');
 const sdk = require('microsoft-cognitiveservices-speech-sdk');
 const request = require('request');
+const axios = require('axios');
 
 async function speakAndChangeVoice(text) {
   const { AudioConfig, SpeechSynthesizer, ResultReason } = require('microsoft-cognitiveservices-speech-sdk');
@@ -15,26 +16,28 @@ async function speakAndChangeVoice(text) {
       async (result) => {
         if (result.reason === ResultReason.SynthesizingAudioCompleted) {
           console.log(result.audioData.byteLength);
-          const request = require('request');
-          const formData = {
-            sample: {
-              value: Buffer.from(result.audioData),
-              options: {
-                filename: 'output.wav',
-                contentType: 'audio/wav'
-              }
-            },
-            fPitchChange: '1',
-            sampleRate: '44100'
-          };
-          const options = {
-            url: 'http://121.41.44.246:7860/voiceChangeModel',
-            formData: formData,
-            headers: { Authorization: process.env.AUTH },
-          };
-          request.post(options, async (error, response, body) => {
-            resolve(Buffer.from(response.data));
+          
+          const axios = require('axios');
+          const FormData = require('form-data');
+          const form = new FormData();
+          form.append('sample', Buffer.from(result.audioData), {
+            filename: 'output.wav',
+            contentType: 'audio/wav'
           });
+          form.append('fPitchChange', '1');
+          form.append('sampleRate', '44100');
+          
+          const headers = {
+            "Content-Type": `multipart/form-data; boundary=${form._boundary}`,
+            Authorization: process.env.AUTH
+          }
+          
+          try {
+            const response = await axios.post('http://121.41.44.246:7860/voiceChangeModel', form, { headers });
+            resolve(response.data);
+          } catch (error) {
+            reject(error);
+          }
         }
         synthesizer.close();
       },
@@ -45,6 +48,7 @@ async function speakAndChangeVoice(text) {
     );
   });
 }
+
 
 module.exports = {
   data: new SlashCommandBuilder()
