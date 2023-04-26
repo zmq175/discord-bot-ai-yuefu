28c8f769c643604dd02426c752280825807dca7a
Chengzhi <zmq175@qq.com>
2023-04-26 22:42:13 +0800

    add mimetype

--- commands/speak.js
+++ commands/speak.js
@@ -65,6 +65,6 @@ module.exports = {
         });
 
         const buffer = await speakAndChangeVoice(text);
-        await interaction.editReply({ files: [{ attachment: buffer}] });
+        await interaction.editReply({ files: [{ attachment: buffer, name: 'output.wav', contentType: 'audio/x-wav' }] });
     }
 }
